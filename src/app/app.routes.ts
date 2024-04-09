e385d581b1c211fb59b3d3f512d61fb411242492
Guilherme gentile <guilherme.gentile@outlook.com.br>
2024-04-09 19:29:21 -0300

    Implement guard to test in home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,18 @@
 import { Routes } from '@angular/router';
+import { authGuard } from './shared/services/auth/auth.guard';
 
 export const routes: Routes = [
   {
     path: 'home',
     loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
+    canActivate: [authGuard]
   },
   {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full',
-  },  {
+  },
+  {
     path: 'login',
     loadComponent: () => import('./pages/login/login.page').then( m => m.LoginPage)
   },
--- src/app/shared/services/auth/auth.guard.ts
+++ src/app/shared/services/auth/auth.guard.ts
@@ -1,21 +1,24 @@
 import { inject } from '@angular/core';
-import { CanActivateFn } from '@angular/router';
+import { CanActivateFn, Router } from '@angular/router';
 import { AuthService } from './auth.service';
 import { catchError, map, take } from 'rxjs/operators';
 import { of } from 'rxjs';
 
 export const authGuard: CanActivateFn = (route, state) => {
   const authService = inject(AuthService);
+  const router = inject(Router);
   
   return authService.isAuthenticated().pipe(
     take(1),
     map(isAuthenticated => {
       if (!isAuthenticated) {
+        router.navigate(['/login']);
         return false;
       }
       return true;
     }),
     catchError(() => {
+      router.navigate(['/login']);
       return of(false);
     })
   );
