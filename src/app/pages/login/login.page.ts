1f39fdafa7dcd45c2ea2e975f43fabef19143474
Guilherme gentile <guilherme.gentile@outlook.com.br>
2024-04-09 20:58:06 -0300

    Login to xmpp server

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,22 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonContent, IonHeader, IonTitle, IonToolbar, IonIcon, IonCard } from '@ionic/angular/standalone';
 import { LoginComponent } from "../../shared/components/login/login.component";
+import { AuthService } from 'src/app/shared/services/auth/auth.service';
 
 @Component({
     selector: 'app-login',
     templateUrl: './login.page.html',
     styleUrls: ['./login.page.scss'],
     standalone: true,
-    imports: [IonCard, IonIcon, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, LoginComponent]
+    imports: [IonContent, LoginComponent]
 })
-export class LoginPage implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-    console.log('login');
-  }
+export class LoginPage {
 
 }
--- src/app/shared/components/login/login.component.ts
+++ src/app/shared/components/login/login.component.ts
@@ -1,19 +1,103 @@
-import { Component, OnInit } from '@angular/core';
-import { IonContent, IonCard, IonCardHeader, IonCardTitle, IonCardContent, IonRow, IonCol, IonButton, IonInput, IonText } from "@ionic/angular/standalone";
+import { UserPreferenceService } from './../../services/user-preference/user-preference.service';
+import { Component, OnInit, inject } from '@angular/core';
+import { IonContent, IonCard, IonCardHeader, IonCardTitle, IonCardContent, IonRow, IonCol, IonButton, IonInput, IonText, ToastController, IonCheckbox, IonLabel, IonItem, IonIcon, NavController } from "@ionic/angular/standalone";
+import { AuthService } from '../../services/auth/auth.service';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { LoginModel } from '../../models/login.model';
+import { PreferencesKey } from '../../enums/preferences.enun';
+import { Router } from '@angular/router';
+import { addIcons } from 'ionicons';
+import { eye, eyeOff } from 'ionicons/icons';
 
 @Component({
   selector: 'app-login-component',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
   standalone: true,
-  imports: [IonText, IonInput, IonButton, IonCol, IonRow, IonCardContent, IonCardTitle, IonContent, IonCard, IonCardHeader]
+  imports: [IonIcon, IonLabel, 
+    IonText, 
+    IonInput, 
+    IonButton, 
+    IonCol, 
+    IonRow, 
+    IonCardContent, 
+    IonCardTitle, 
+    IonContent, 
+    IonCard, 
+    IonCardHeader, 
+    ReactiveFormsModule,
+    IonCheckbox,
+    IonItem,
+    IonIcon
+  ]
 })
-export class LoginComponent  implements OnInit {
+export class LoginComponent implements OnInit {
+  private authService = inject(AuthService);
+  private userPreferenceService = inject(UserPreferenceService);
+  private navController = inject(NavController);
+  private toastController = inject(ToastController);
 
-  constructor() { }
+  loginForm = new FormGroup({
+    server: new FormControl('', Validators.required),
+    username: new FormControl('', Validators.required),
+    password: new FormControl('', Validators.required),
+    rememberMe: new FormControl(false),
+    autoLogin: new FormControl(false)
+  });
 
-  ngOnInit() {
-    console.log('login');
+  showPassword = false;
+
+  constructor() {
+    addIcons({
+      eye,
+      eyeOff
+    });
   }
 
+  ngOnInit(): void {
+    this.loadPreferences();
+  }
+
+  ionViewDidEnter(){
+    this.loadPreferences();
+  }
+
+  loadPreferences(){
+    const preferences = this.userPreferenceService.getPreference<LoginModel>(PreferencesKey.UserCredentials);
+
+    if(preferences?.rememberMe){
+      this.loginForm.setValue(preferences);
+    }
+  }
+
+  login(): void {
+    this.loginForm.markAllAsTouched();
+
+    if(this.loginForm.valid){
+      this.authService.login(this.loginForm.value as LoginModel).subscribe((logged) => {
+        if(!logged){
+          this.presentErrorToast();
+          return;
+        }
+        
+        this.navController.navigateRoot('/home');
+      });
+    }
+   
+  }
+
+  async presentErrorToast() {
+    const toast = await this.toastController.create({
+      message: 'Ocorreu um erro ao tentar logar. Verifique as credenciais e tente novamente.',
+      duration: 1500,
+      position: 'bottom',
+      color: 'danger'
+    });
+
+    await toast.present();
+  }
+
+  togglePassword() {
+    this.showPassword = !this.showPassword;
+  }
 }
