a4da11bf435444d0ac4d8ca3302d856a84a1ccb9
Guilherme Gentile <uk.gentile@gmail.com>
2024-04-10 14:32:10 -0300

    Installed Capacitor

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'vibe-chat',
+  appId: 'com.vibe.chat',
+  appName: 'VibeChat',
   webDir: 'www',
   server: {
     androidScheme: 'https'
