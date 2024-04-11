e17082d374638cc2c5b90b83b42d724120f1828c
Guilherme Gentile <uk.gentile@gmail.com>
2024-04-11 15:26:35 -0300

    Initialize db on appcomponenet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
+import { DatabaseService } from './shared/services/database/database.service';
 
 @Component({
   selector: 'app-root',
@@ -8,5 +9,6 @@ import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
   imports: [IonApp, IonRouterOutlet],
 })
 export class AppComponent {
+  private db = inject(DatabaseService);
   constructor() {}
 }
