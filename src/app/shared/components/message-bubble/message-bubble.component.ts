fb3d3cb8ee3ebce6ae05e0840e6b09e132164e3c
CAIQUE2SILVA <64806656+CAIQUE2SILVA@users.noreply.github.com>
2024-04-12 18:31:25 -0300

    ajustando o bubble , nao terminado ainda .

--- src/app/shared/components/message-bubble/message-bubble.component.ts
+++ src/app/shared/components/message-bubble/message-bubble.component.ts
@@ -2,16 +2,21 @@ import { Component, Input, OnInit, inject } from '@angular/core';
 import { MessageModel } from '../../models/message.model';
 import { CommonModule } from '@angular/common';
 import { ChatRepository } from '../../repositories/chat/chat.repository';
+import { IonItem } from "@ionic/angular/standalone";
+import { AvatarComponent } from '../avatar/avatar.component';
 
 @Component({
   selector: 'app-message-bubble',
   templateUrl: './message-bubble.component.html',
   styleUrls: ['./message-bubble.component.scss'],
   standalone: true,
-  imports: [CommonModule],
+  imports: [IonItem, CommonModule,AvatarComponent],
 })
-export class MessageBubbleComponent {
+export class MessageBubbleComponent implements OnInit {
   @Input() message!: MessageModel;
 
   constructor() { }
+
+  ngOnInit(): void {console.log(this.message)};
+
 }
