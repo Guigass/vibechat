c78ee5fbc8cd14608331fdf563125c0746153fb5
CAIQUE2SILVA <64806656+CAIQUE2SILVA@users.noreply.github.com>
2024-04-12 14:41:19 -0300

    adicionado a logo

--- src/app/pages/app/app.page.ts
+++ src/app/pages/app/app.page.ts
@@ -1,7 +1,7 @@
 import { SplashScreenService } from './../../shared/services/splash-screen/splash-screen.service';
 import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { IonApp, IonSplitPane, IonMenu, IonContent, IonRouterOutlet, IonHeader, IonToolbar, IonTitle, IonFooter } from '@ionic/angular/standalone';
+import { IonApp, IonSplitPane, IonMenu, IonContent, IonRouterOutlet, IonHeader, IonToolbar, IonTitle, IonFooter, IonItem, IonImg } from '@ionic/angular/standalone';
 import { AsideComponent } from 'src/app/shared/components/aside/aside.component';
 import { TabsComponent } from 'src/app/shared/components/tabs/tabs.component';
 import { DatabaseService } from 'src/app/shared/services/database/database.service';
@@ -12,12 +12,12 @@ import { debounceTime } from 'rxjs';
   templateUrl: './app.page.html',
   styleUrls: ['./app.page.scss'],
   standalone: true,
-  imports: [IonTitle, IonToolbar, IonHeader, 
-    CommonModule, 
-    IonApp, 
-    IonSplitPane, 
-    IonMenu, 
-    IonContent, 
+  imports: [IonImg, IonItem, IonTitle, IonToolbar, IonHeader,
+    CommonModule,
+    IonApp,
+    IonSplitPane,
+    IonMenu,
+    IonContent,
     IonRouterOutlet,
     AsideComponent,
     TabsComponent,
