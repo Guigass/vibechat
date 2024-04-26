089588b4fd1d81aad1d6b545cf7f2810e3dd7993
CAIQUE2SILVA <64806656+CAIQUE2SILVA@users.noreply.github.com>
2024-04-26 11:36:59 -0300

    creation: image bubble

--- src/app/shared/components/image-bubble/image-bubble.component.ts
+++ src/app/shared/components/image-bubble/image-bubble.component.ts
@@ -1,14 +1,17 @@
 import { Component, Input, OnInit, input } from '@angular/core';
 import { MessageModel } from '../../models/message.model';
 import { ContactModel } from '../../models/contact.model';
-import { IonImg } from "@ionic/angular/standalone";
+import { IonImg, IonItem } from "@ionic/angular/standalone";
+import { AvatarComponent } from "../avatar/avatar.component";
+import { ModalController } from '@ionic/angular/standalone';
+import { ImageModalComponent } from './image-modal/image-modal.component';
 
 @Component({
-  selector: 'app-image-bubble',
-  templateUrl: './image-bubble.component.html',
-  styleUrls: ['./image-bubble.component.scss'],
-  standalone: true,
-  imports:[IonImg,]
+    selector: 'app-image-bubble',
+    templateUrl: './image-bubble.component.html',
+    styleUrls: ['./image-bubble.component.scss'],
+    standalone: true,
+    imports: [IonImg, IonItem, AvatarComponent]
 })
 export class ImageBubbleComponent  implements OnInit {
 
@@ -16,8 +19,22 @@ export class ImageBubbleComponent  implements OnInit {
 contact = input<ContactModel>();
 message = input<MessageModel>();
 
-  constructor() { }
+  constructor(
+    private modalCtrl: ModalController
+  ) { }
 
   ngOnInit() {}
+  async openImage() {
+      const modal = await this.modalCtrl.create({
+          component: ImageModalComponent,
+          componentProps: {image: this.message()?.body},
+      });
+      modal.onWillDismiss().then((data) => {
+        if(data.data) {
+          // console.log(data.data)
+        }
+      })
+      return await modal.present();
+  }
 
 }
